9b7fee738f23e1fa338211087aa1037cb6df9f99
JoseRaiders <71295774+JoseRaiders@users.noreply.github.com>
2025-04-04 19:24:35 -0500

    fix: controllers/users.js

--- controllers/users.js
+++ controllers/users.js
@@ -8,11 +8,11 @@ const {
 } = require("../utils/errors");
 
 const getUsers = (req, res) => {
-  User.find({})
+  return User.find({})
     .then((users) => res.status(OK).send({ data: users }))
     .catch((err) => {
       console.error(err);
-      res
+      return res
         .status(INTERNAL_SERVER_ERROR)
         .send({ message: "Error getting users" });
     });
@@ -25,7 +25,7 @@ const getUser = (req, res) => {
     return res.status(BAD_REQUEST).send({ message: "User ID is required" });
   }
 
-  User.findById(userId)
+  return User.findById(userId)
     .then((user) => {
       if (!user) {
         return res.status(NOT_FOUND).send({ message: "User not found" });
@@ -43,7 +43,7 @@ const getUser = (req, res) => {
 const createUser = (req, res) => {
   const { name, avatar } = req.body;
 
-  User.create({ name, avatar })
+  return User.create({ name, avatar })
     .then((user) => res.status(CREATED).send({ data: user }))
     .catch((err) => {
       console.error(err);
