53f53d7b41551eb2f0cddbf4d465b9da05292fc2
JoseRaiders <71295774+JoseRaiders@users.noreply.github.com>
2025-04-05 09:48:32 -0500

    Add middleware for handling an unknown route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
+const { NOT_FOUND } = require("../utils/errors");
 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
+
+router.use((req, res) => {
+  res.status(NOT_FOUND).send({ error: "Route not found" });
+});
 
 module.exports = router;
