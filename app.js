76b819392e642aa6afa0dfc49975a0b55e409bea
JoseRaiders <71295774+JoseRaiders@users.noreply.github.com>
2025-04-27 10:18:01 -0500

    drop: hard-coded user object, request pass thought auth.js middleware

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ mongoose
   })
   .catch((err) => console.error(err));
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: "67eb42c90a0f10ee7a249aed",
-  };
-  next();
-});
+// app.use((req, res, next) => {
+//   req.user = {
+//     _id: "67eb42c90a0f10ee7a249aed",
+//   };
+//   next();
+// });
 
 app.use(express.json());
 app.use("/", mainRouter);
@@ -25,5 +25,3 @@ app.use("/", mainRouter);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}...`);
 });
-
-// owner ID 67eb42c90a0f10ee7a249aed
