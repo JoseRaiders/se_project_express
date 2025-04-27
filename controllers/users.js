8c25c97febbf148601610ffcf77b003de5d6dae8
JoseRaiders <71295774+JoseRaiders@users.noreply.github.com>
2025-04-27 09:13:01 -0500

    update createUser controller to handle pwd hashing and email errors

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,65 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const {
   OK,
   CREATED,
   BAD_REQUEST,
+  UNAUTHORIZED_ERROR,
   NOT_FOUND,
+  CONFLICT_ERROR,
   INTERNAL_SERVER_ERROR,
 } = require("../utils/errors");
+
+const createUser = (req, res) => {
+  const { name, avatar, email, password } = req.body;
+
+  bcrypt
+    .hash(password, 10)
+    .then((hash) =>
+      User.create({
+        name,
+        avatar,
+        email,
+        password: hash,
+      })
+    )
+    .then((user) => res.status(CREATED).send({ data: user }))
+    .catch((err) => {
+      console.error(err);
+      if (err.code === 11000) {
+        return res
+          .status(CONFLICT_ERROR)
+          .send({ message: "User with this email already exists" });
+      }
+      if (err.name === "ValidationError") {
+        return res.status(BAD_REQUEST).send({ message: err.message });
+      }
+      if (err.name === "CastError") {
+        return res
+          .status(BAD_REQUEST)
+          .send({ message: "Invalid input format" });
+      }
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "Error creating the user" });
+    });
+};
+
+const loginUser = (req, res) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      res.send({ token });
+    })
+    .catch((err) => {
+      res.status(UNAUTHORIZED_ERROR).send({ message: err.message });
+    });
+};
 
 const getUsers = (req, res) => {
   User.find({})
@@ -44,29 +98,9 @@ const getUser = (req, res) => {
     });
 };
 
-const createUser = (req, res) => {
-  const { name, avatar } = req.body;
-
-  return User.create({ name, avatar })
-    .then((user) => res.status(CREATED).send({ data: user }))
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "ValidationError") {
-        return res.status(BAD_REQUEST).send({ message: err.message });
-      }
-      if (err.name === "CastError") {
-        return res
-          .status(BAD_REQUEST)
-          .send({ message: "Invalid input format" });
-      }
-      return res
-        .status(INTERNAL_SERVER_ERROR)
-        .send({ message: "Error creating the user" });
-    });
-};
-
 module.exports = {
+  createUser,
+  loginUser,
   getUsers,
   getUser,
-  createUser,
 };
